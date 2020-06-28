5a8a6c4a97b37bfcbe07beaad132ebbf3cf2ddd7
pointlessrapunzel <vladace9@gmail.com>
2020-06-28 21:52:52 +0500

    submit ex 1.12-13, anecdotes step1-2

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const Anecdote = ({ selected, votes }) => {
+  return (
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes} votes</p>
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
 
 const App = () => {
+  const [ selected, setSelected ] = useState(0)
+  const [ votes, setVotes ] = useState(anecdotes.map(a => 0))
+
+  const pickRandomAnecdote = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+
+  const handleUpvote = () => {
+    setVotes(votes.map((v, i) => i === selected ? v + 1 : v))
+  }
+
   return (
     <div className="App">
+      <Anecdote selected={selected} votes={votes[selected]} />
+      <Button 
+        handleClick={handleUpvote} 
+        text={'vote'} 
+       />
+      <Button 
+        handleClick={pickRandomAnecdote} 
+        text={'next anecdote'} 
+       />
     </div>
   );
 }
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
 export default App;
