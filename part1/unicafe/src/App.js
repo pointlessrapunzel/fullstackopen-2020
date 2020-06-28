3d99747a362b30b1f86bb2431fef258d9221e89f
pointlessrapunzel <vladace9@gmail.com>
2020-06-28 18:08:19 +0500

    submit ex 1.6, unicafe step1

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function App() {
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
+
+const Stat = ({ value, text }) => {
+  return (
+    <p>{text} {value}</p>
+  )
+}
+
+const Stats = ({ stats }) => {
+  return (
+    <>
+      {stats.map((stat, i) => 
+        <Stat key={i} value={stat.value} text={stat.text} /> 
+      )}
+    </>
+  )
+}
+
+const App = () => {
+  const [ good, setGood ] = useState(0)
+  const [ neutral, setNeutral ] = useState(0)
+  const [ bad, setBad ] = useState(0)
+
+  const stats = [ 
+  {
+    text: 'good',
+    value: good
+  }, 
+  {
+    text: 'neutral',
+    value: neutral
+  },
+  {
+    text: 'bad',
+    value: bad
+  }]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>give feedback</h2>
+      <Button handleClick={() => setGood(good + 1)} text='good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <h2>statistics</h2>
+      <Stats stats={stats} />
     </div>
   );
 }
