c4e2d51f6944a35444a3ac9f525fe37874a46920
pointlessrapunzel <vladace9@gmail.com>
2020-06-29 19:14:01 +0500

    submit ex2.6, phonebook step1

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,8 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const NumbersList = ({persons}) => {
+  return (
+    <ul style={{'listStyle': 'none'}}>
+      {persons.map(p => <li key={p.name}>{p.name}</li>)}
+    </ul>
+  )
+}
 
 const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [ newName, setNewName ] = useState('')
+
+  const handleAddPerson = e => {
+    e.preventDefault()
+    console.log(newName)
+    if (persons.map(p => p.name).includes(newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+
+    setPersons(persons.concat(
+      { name: newName }
+    ))
+    setNewName('')
+  }
+
+  const handleNewNameChange = e => {
+    setNewName(e.target.value)
+  }
+
   return (
     <div className="App">
+      <h2>Phonebook</h2>
+      <form onSubmit={handleAddPerson}>
+        <div>
+          name: <input 
+            onChange={handleNewNameChange}
+            value={newName}
+          />
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <NumbersList persons={persons} />
     </div>
   );
 }
