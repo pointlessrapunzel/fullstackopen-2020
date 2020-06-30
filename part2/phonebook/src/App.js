4b70dbe309b2fb1e1fb716dfeb1a22c8705b76d8
pointlessrapunzel <vladace9@gmail.com>
2020-07-01 03:26:03 +0500

    submit ex2.11, phonebook step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+
 import Input from './components/Input'
 import PersonForm from "./components/PersonForm";
 
@@ -14,19 +16,21 @@ const Persons = ({persons}) => {
 }
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [ persons, setPersons ] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter ] = useState('')
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
   const handleAddPerson = e => {
     e.preventDefault()
-    console.log(newName)
     if (persons.map(p => p.name).includes(newName)) {
       alert(`${newName} is already added to phonebook`)
       return
