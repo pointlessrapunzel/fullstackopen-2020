1f07b6a3016937356833ed76915d10d7a1af8730
pointlessrapunzel <vladace9@gmail.com>
2020-07-08 20:21:11 +0500

    Submit ex 4.4, unit tests, step2
    Move dummy.test to list_helper.test, add totalLikes to utils/list_helper.js and test for it

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -2,4 +2,8 @@ const dummy = blogs => {
   return 1
 }
 
-module.exports = { dummy }
+const totalLikes = blogs => {
+  return blogs.reduce((total, blog) => total + blog.likes, 0)
+}
+
+module.exports = { dummy, totalLikes }
