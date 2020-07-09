594268f1c527376be5b576d6a86975bd49cf5cd8
pointlessrapunzel <vladace9@gmail.com>
2020-07-09 12:10:10 +0500

    Submit ex 4.6, helper functions atep4
    Add mostBlogs helper function and a tests for it

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -1,17 +1,36 @@
-const dummy = blogs => {
+const dummy = (blogs) => {
   return 1
 }
 
-const totalLikes = blogs => {
+const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + blog.likes, 0)
 }
 
-const favoriteBlog = blogs => {
+const favoriteBlog = (blogs) => {
   if (blogs.length === 0) return null
-  
-  return blogs.reduce((mostLikedBlog, blog) => 
+
+  return blogs.reduce((mostLikedBlog, blog) =>
     blog.likes > mostLikedBlog.likes ? blog : mostLikedBlog
   )
 }
 
-module.exports = { dummy, totalLikes, favoriteBlog }
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null
+
+  let mostBlogsAuthor = blogs[0].author
+
+  const blogsByAuthor = blogs.reduce((blogsByAuthor, blog) => {
+    // for each blog, add one to the total for its author
+    blogsByAuthor[blog.author] = blogsByAuthor[blog.author] + 1 || 1
+
+    // update the max author if the current author has more blogs
+    if (blogsByAuthor[blog.author] > blogsByAuthor[mostBlogsAuthor]) {
+      mostBlogsAuthor = blog.author
+    }
+    return blogsByAuthor
+  }, {})
+
+  return { author: mostBlogsAuthor, blogs: blogsByAuthor[mostBlogsAuthor] }
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs }
