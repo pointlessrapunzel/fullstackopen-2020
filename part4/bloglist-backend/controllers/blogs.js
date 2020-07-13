dbaa17d6077aff091b6b905d39a56e6e745435d5
pointlessrapunzel <vladace9@gmail.com>
2020-07-13 17:11:47 +0500

    Submit ex 4.17, bloglist exp, step5
    Modify adding blogs to also add an arbitrary user id to it

--- part4/bloglist-backend/controllers/blogs.js
+++ part4/bloglist-backend/controllers/blogs.js
@@ -1,20 +1,29 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (req, res, next) => {
-
   try {
-    const blogs = await Blog.find({})
+    const blogs = await Blog
+      .find({})
+      .populate('user', { username: 1, name: 1 })
     res.json(blogs)
   } catch(e) { next(e) }
 })
 
 blogsRouter.post('/', async (req, res, next) => {
+  const body = req.body
+  const user = await User.findOne({})
   try {
-    if (!req.body.title || !req.body.url) {
+    if (!body.title || !body.url) {
       return res.status(400).send({ error: 'Missing title or url' })
     }
-    const blog = new Blog(req.body)
+
+    const blog = new Blog({
+      ...body,
+      user: user._id
+    })
+
     if (!blog.likes) blog.likes = 0
     const savedBlog = await blog.save()
     res.status(201).json(savedBlog)
