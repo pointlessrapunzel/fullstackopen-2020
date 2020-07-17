a2052a9af25104d75d6aa46c1321e9c6a2f9110c
pointlessrapunzel <vladace9@gmail.com>
2020-07-17 14:47:33 +0500

    Submit ex 5.7, make blogs expandable
    Add view/hide button, that will reveal the details of the blog

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,27 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [shown, setShown] = useState(false)
+
+  const toggleShown = () => setShown(!shown)
+  const handleLike = () => console.log(blog.title, 'liked!')
+
+  const renderDetails = () => (
+    <div>
+      {blog.url}
+      <br />
+      likes {blog.likes}
+      <button onClick={handleLike}>like</button>
+      <br />
+      {blog.user.name}
+    </div>
+  )
+
+  return (
+    <div className='blog'>
+      {blog.title} {blog.author}
+      <button onClick={toggleShown}>{shown ? 'hide' : 'view'}</button>
+      {shown && renderDetails()}
+    </div>
+)}
 
 export default Blog
