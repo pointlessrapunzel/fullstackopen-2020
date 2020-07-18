a2b141d2cc185408d737d34350c9e947d6ba9005
pointlessrapunzel <vladace9@gmail.com>
2020-07-18 22:27:28 +0500

    Submit ex 5.14, bloglist tests 2
    Add test to check that blogs details are shown after clicking the view button

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 const testBlog = {
   title: 'Test Blog',
   author: 'Test Blog Author',
   likes: 10,
-  url: '//url'
+  url: '//url',
+  user: {
+    name: 'Test User',
+    username: 'testuser'
+  }
 }
 
-test('renders content', () => {
+test('Correctly renders content, title and author shown, likes and url are not', () => {
   const component = render(
     <Blog blog={testBlog} />
   )
@@ -20,4 +24,17 @@ test('renders content', () => {
   )
   expect(component.container).not.toHaveTextContent('likes 10')
   expect(component.container).not.toHaveTextContent('//url')
+})
+
+test('Likes and url are shown after the view button clicked', () => {
+  const component = render(
+    <Blog blog={testBlog} />
+  )
+
+  const button = component.container.querySelector('button')
+  fireEvent.click(button)
+
+  component.debug()
+  expect(component.container).toHaveTextContent('likes 10')
+  expect(component.container).toHaveTextContent('//url')
 })
