02217a352b279da6b6e5afa0a51d596f2eb53222
pointlessrapunzel <savivalto@gmail.com>
2023-04-15 22:11:10 +0500

    Ex 9.9: add cors

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
+app.use((cors as (options: cors.CorsOptions) => express.RequestHandler)({}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
