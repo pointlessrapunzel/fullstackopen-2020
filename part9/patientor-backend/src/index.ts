ca9fcfd5851f51e8a22c03561faf17815b415595
pointlessrapunzel <savivalto@gmail.com>
2023-04-17 16:56:36 +0500

    Ex 9.10: Add /api/diagnoses endpoint, data and data type

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import diagnosesRouter from "./routes/diagnoses";
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/api/ping", (_, res) => {
   res.send("pong");
 });
+
+app.use("/api/diagnoses", diagnosesRouter);
 
 const PORT = 3001;
 
