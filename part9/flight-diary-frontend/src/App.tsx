101511bb7a4acc6d319dd81ef00ca8c94354bbd1
pointlessrapunzel <savivalto@gmail.com>
2023-04-29 15:14:10 +0500

    Ex 9.17: Add new diary form

--- part9/flight-diary-frontend/src/App.tsx
+++ part9/flight-diary-frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { getAllDiaries, postNewDiary } from "./services/diaryService";
 
 type Diary = {
   id: string;
@@ -11,14 +12,34 @@ function App() {
   const [diaries, setDiaries] = useState<Diary[]>([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/api/diaries")
-      .then<Diary[]>((res) => res.json())
-      .then((json) => setDiaries(json));
+    getAllDiaries()
+      .then((json) => setDiaries(json))
+      .catch((e) => console.error(e));
   }, []);
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const data = new FormData(e.currentTarget);
+    postNewDiary(Object.fromEntries(data))
+      .then((savedDiary) => {
+        // successfull
+        setDiaries(diaries.concat(savedDiary));
+      })
+      .catch((err) => {
+        // unsuccessfull
+        // show error message
+        console.error(err);
+      });
+  }
 
   return (
     <>
       <h1>Ilari&apos;s flight diaries</h1>
+      <section>
+        <h2>add new entry</h2>
+        <AddDiaryForm onSubmit={handleSubmit} />
+      </section>
       <section>
         <h2>Diary entries</h2>
         <DiariesList diaries={diaries} />
@@ -49,6 +70,36 @@ function DiaryItem({ item }: { item: Diary }) {
         <span>weather: {item.weather}</span>
       </div>
     </>
+  );
+}
+
+type DiaryFormProps = {
+  onSubmit: React.FormEventHandler<HTMLFormElement>;
+};
+
+function AddDiaryForm({ onSubmit }: DiaryFormProps) {
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        <label htmlFor="date">date</label>
+        <input type="date" name="date" id="date" />
+      </div>
+      <div>
+        <label htmlFor="visibility">visibility</label>
+        <input type="text" name="visibility" id="visibility" />
+      </div>
+      <div>
+        <label htmlFor="weather">weather</label>
+        <input type="text" name="weather" id="weather" />
+      </div>
+      <div>
+        <label htmlFor="comment">comment</label>
+        <input type="text" name="comment" id="comment" />
+      </div>
+      <div>
+        <input type="submit" value="add" />
+      </div>
+    </form>
   );
 }
 
